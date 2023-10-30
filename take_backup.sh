b804e0ce4c8614bded8a36b1651ba6f89965b925
juaramirez <juan.jramirez@mercadolibre.com.co>
2023-10-30 18:29:26 -0500

    adding taking backup

--- take_backup.sh
+++ take_backup.sh
@@ -2,3 +2,6 @@
 mdfind kMDItemKind="Application" > ~/dotfiles/apps/apps_all.txt
 mdfind -onlyin /Applications -onlyin ~/Applications kMDItemKind="Application" > ~/dotfiles/apps/apps.txt
 brew bundle dump --force --file=~/dotfiles/apps/Brewfile
+brew list  > ~/dotfiles/apps/brew_list.txt 
+brew list --cask > ~/dotfiles/apps/brew_listCask.txt 
+brew deps --tree --installed  > ~/dotfiles/apps/brew_depsTree.txt
