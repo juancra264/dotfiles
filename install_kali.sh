53d203d822d3294bb13cf8a3ee5111c8569e215d
Juan Carlos Ramirez Angel <juancra264@hotmail.com>
2025-08-19 12:10:17 -0500

    update

--- install_kali.sh
+++ install_kali.sh
@@ -168,10 +168,8 @@ case "$OS" in
     echo "##################################################################"
     sudo apt-get update
     sudo apt-get install ca-certificates curl gnupg -y
-    #curl -sSL https://pkgs.netbird.io/debian/public.key 
-    #sudo gpg --dearmor --output /usr/share/keyrings/netbird-archive-keyring.gpg
-    #echo 'deb [signed-by=/usr/share/keyrings/netbird-archive-keyring.gpg] https://pkgs.netbird.io/debian stable main'
-    #sudo tee /etc/apt/sources.list.d/netbird.list
+    curl -sSL https://pkgs.netbird.io/debian/public.key | sudo gpg --dearmor --output /usr/share/keyrings/netbird-archive-keyring.gpg
+    echo 'deb [signed-by=/usr/share/keyrings/netbird-archive-keyring.gpg] https://pkgs.netbird.io/debian stable main' | sudo tee /etc/apt/sources.list.d/netbird.list
     sudo apt-get update -y
     sudo apt-get install netbird -y
     sudo apt-get install netbird-ui -y
