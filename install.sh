f95b898b39580b37fada1f5d1d0dfb774d3aec4f
juancra264 <juancra264@hotmail.com>
2025-09-24 23:09:27 -0500

    adding ncdu install

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ f_linux_basic_packages() {
   echo "${blue} Installing basic packages${reset}"
   echo "${blue}###############################################################################${reset}"
   sudo apt install glances bmon htop speedtest-cli mosh eza picocom -y 
-  sudo apt install wireshark git tmux python3 python3-pip tlp jq  -y
+  sudo apt install ncdu wireshark git tmux python3 python3-pip tlp jq  -y
   sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions -y
 }
 
