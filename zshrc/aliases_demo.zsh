6433377349bea99318676e60ac754ff12a7adc24
Juan Carlos Ramirez Angel <juancra264@hotmail.com>
2025-09-30 16:14:55 -0500

    adding aliases example

---
+++ zshrc/aliases_demo.zsh
@@ -0,0 +1,11 @@
+alias python="python3"
+alias ws_start="tmux has-session -t ws 2>/dev/null || tmux new-session -d -s ws"
+alias ws="tmux a -t ws"
+alias pingmon="python ~/nettools/pingmon/pingmon.py"
+alias macvendor="bash ~/nettools/macvendor/macvendor.sh"
+alias macadd="bash ~/nettools/macadd/macadd.sh"
+alias checknet="bash ~/nettools/checknet/checknet.sh"
+alias ipaddinfo="bash ~/nettools/ipaddinfo/ipaddinfo.sh"
+alias ls="eza"
+
+s
